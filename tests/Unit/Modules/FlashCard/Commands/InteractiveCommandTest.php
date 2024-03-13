42c0d46bbb29512c8eceb8953d4b910efe3e0099
Mohammad <mohammad@Mohammads-MacBook-Pro.local>
2024-03-13 15:18:05 +0200

    TDD: write test for IntractiveCommand

---
+++ tests/Unit/Modules/FlashCard/Commands/InteractiveCommandTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit\Modules\FlashCard\Commands;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Artisan;
+use App\Modules\FlashCard\Interfaces\MainMenoActionInterface;
+
+class InteractiveCommandTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function it_calls_main_meno_action()
+    {
+        //Arrange
+        $mock = $this->mock(MainMenoActionInterface::class, function ($mock) {
+            $mock->shouldReceive('__invoke')->once();
+        });
+
+        $this->app->instance(MainMenoActionInterface::class, $mock);
+
+        //ACT
+        Artisan::call('flashcard:interactive');
+    }
+}
