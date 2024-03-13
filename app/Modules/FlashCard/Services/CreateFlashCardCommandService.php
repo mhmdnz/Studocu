c5c02628696752aa08ebb701c9fd009f9ea3ff1e
Mohammad <mohammad@Mohammads-MacBook-Pro.local>
2024-03-13 19:55:29 +0200

    Add the CreateFlashCardCommandService to cover the test

---
+++ app/Modules/FlashCard/Services/CreateFlashCardCommandService.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace app\Modules\FlashCard\Services;
+
+use App\Modules\FlashCard\Bridges\CommandServiceBridge;
+use Illuminate\Support\Facades\Validator;
+
+class CreateFlashCardCommandService extends CommandServiceBridge
+{
+    public function __invoke(): void
+    {
+        $question = $this->command->ask('Enter flash card question?');
+        $answer = $this->command->ask('Enter flash card answer?');
+
+        $data = [
+            'question' => $question,
+            'answer' => $answer,
+        ];
+
+        $validator = Validator::make($data, [
+            'question' => 'required|string|max:100|min:5|unique:flash_cards',
+            'answer' => 'required|string|max:100|min:5',
+        ]);
+
+        if ($validator->fails()) {
+            $this->command->error(implode(", ", $validator->errors()->all()));
+        }
+
+        $this->flashCardRepository->create($question, $answer);
+    }
+}
+
