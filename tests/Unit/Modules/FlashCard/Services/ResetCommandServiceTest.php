018c954bd7a791288a049de14dac847afbfdba8a
Mohammad <mohammad@Mohammads-MacBook-Pro.local>
2024-03-13 20:23:07 +0200

    TDD : add ResetCommandServiceTest so that I can write ResetCommandService

--- tests/Unit/Modules/FlashCard/Services/ResetCommandServiceTest.php
+++ tests/Unit/Modules/FlashCard/Services/ResetCommandServiceTest.php
@@ -4,31 +4,30 @@ namespace Tests\Unit\Modules\FlashCard\Services;
 
 use App\Modules\FlashCard\Interfaces\FlashCardRepositoryInterface;
 use App\Modules\FlashCard\Interfaces\UserRepositoryInterface;
-use App\Modules\FlashCard\Services\ListCardsCommandService;
+use App\Modules\FlashCard\Services\ResetCommandService;
 use Illuminate\Console\Command;
 use Tests\TestCase;
 use Mockery;
 
-class ListCardsCommandServiceTest extends TestCase
+class ResetCommandServiceTest extends TestCase
 {
     /** @test  */
     public function invoke_correctly()
     {
-        //Arrange
-        $mockedFields = ['id', 'question', 'answer'];
         $userRepositoryMock = Mockery::mock(UserRepositoryInterface::class);
-
-        $flashCardRepositoryMock = Mockery::mock(FlashCardRepositoryInterface::class);
-        $flashCardRepositoryMock->shouldReceive('getList')
-            ->with($mockedFields)
-            ->once()->andReturn(collect());
-
-        $commandMock = Mockery::mock(Command::class);
-        $commandMock->shouldReceive('table')
+        $userRepositoryMock->shouldReceive('detachAllFlashCards')
             ->once()
             ->andReturn();
 
-        $service = new ListCardsCommandService($commandMock, $flashCardRepositoryMock, $userRepositoryMock);
+        $flashCardRepositoryMock = Mockery::mock(FlashCardRepositoryInterface::class);
+
+        $commandMock = Mockery::mock(Command::class);
+        $commandMock->shouldReceive('info')
+            ->with('All answered removed!!')
+            ->once()
+            ->andReturn();
+
+        $service = new ResetCommandService($commandMock, $flashCardRepositoryMock, $userRepositoryMock);
 
         //ACT
         $service();
